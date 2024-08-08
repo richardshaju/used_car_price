bef6228927b56fd5b9581b49ea64e19757cf9a99
Richard Shaju <richardshaju66@gmail.com>
2024-08-09 00:31:17 +0530

    new API added

--- client/src/Home/Home.jsx
+++ client/src/Home/Home.jsx
@@ -32,7 +32,7 @@ function Home() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    fetch("http://127.0.0.1:5000/predict_car_price", {
+    fetch("https://used-car-price-g5fz.onrender.com/predict_car_price", {
       method: "POST",
       mode: 'cors',
       body: JSON.stringify({
@@ -56,7 +56,7 @@ function Home() {
   };
 
   useEffect(() => {
-    fetch("http://127.0.0.1:5000/get_brand")
+    fetch("https://used-car-price-g5fz.onrender.com/get_brand")
       .then((response) => response.json())
       .then((data) => setData(data.brand))
       .catch((error) => console.error("error:" + error));
@@ -91,7 +91,7 @@ function Home() {
                   />
                   {isOpenBrand && (
                     <div className="option">
-                      {data?.map((item) => (
+                      {data && data?.map((item) => (
                         <p
                           onClick={() => {
                             setBrand(item);
