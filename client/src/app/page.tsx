c59b822b5433d67c0baa4338f3f777acc3480c4f
Richard Shaju <richardshaju66@gmail.com>
2024-08-13 23:32:34 +0530

    form part added

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,113 +1,142 @@
+"use client";
 import Image from "next/image";
+import { Ysabeau_SC } from "next/font/google";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+import { useEffect, useState } from "react";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 
+const font = Ysabeau_SC({ subsets: ["latin"], weight: ["400"] });
 export default function Home() {
+  const [brands, setBrands] = useState<[string]>();
+  const [isValid, setIsValid] = useState(true);
+
+  const years = [];
+  for (let year = 2013; year <= 2022; year++) {
+    years.push(year);
+  }
+
+  useEffect(() => {
+    fetch("https://used-car-price-g5fz.onrender.com/get_brand")
+      .then((response) => response.json())
+      .then((data) => setBrands(data.brand))
+      .catch((error) => console.error("error:" + error));
+  }, []);
+
+  const [formData, setFormData] = useState({
+    brand: "",
+    year: "",
+    kilometer: "",
+    fuelType: "",
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Access the form data here
+    console.log(formData);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className={`w-full h-full flex ${font.className}`}>
+      <div className="w-1/2 h-screen bg-[#efeded] rounded-r-[50px] drop-shadow-2xl ">
+        <div className="p-6 font-bold text-2xl text-center">
+          <h1>USED CAR PRICE PREDICTOR</h1>
+        </div>
+        <div className="flex flex-col justify-center items-center mt-20">
+          <h1 className="text-2xl">Enter the parameters</h1>
+          <form
+            className="flex flex-col gap-2 items-center"
+            onSubmit={handleSubmit}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <Select>
+              <div className="flex flex-col">
+                <p>brand</p>
+                <SelectTrigger className="w-[280px]">
+                  <SelectValue placeholder="Select a brand" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Cars</SelectLabel>
+                    {brands &&
+                      brands.map((brand: string) => (
+                        <SelectItem key={brand} value={brand}>
+                          {brand.toUpperCase()}
+                        </SelectItem>
+                      ))}
+                  </SelectGroup>
+                </SelectContent>
+              </div>
+            </Select>
+
+            <Select>
+              <div className="flex flex-col">
+                <p>Year</p>
+                <SelectTrigger className="w-[280px]">
+                  <SelectValue placeholder="Select Manufacture Year" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Year</SelectLabel>
+                    {years.map((item,idx) => (
+                      <SelectItem
+                        key={item}
+                        value={formData.year}
+                        onClick={() =>
+                          setFormData({ ...formData, year: item.toString() })
+                        }
+                      >
+                        {item.toString()}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </div>
+            </Select>
+
+            <div className="flex flex-col">
+              <p>kilometer</p>
+              <Input
+                className="w-[280px]"
+                placeholder="Driven Kilometer"
+                name="kilometer"
+                value={formData.kilometer}
+                onChange={handleInputChange}
+              />
+              {isValid ? null : <span>should be between 5000 & 100000</span>}
+            </div>
+            <Select>
+              <div className="flex flex-col">
+                <p>fuel type</p>
+                <SelectTrigger className="w-[280px]">
+                  <SelectValue placeholder="Select a fuel type" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Fuel type</SelectLabel>
+                    <SelectItem value="akst">DIESEL</SelectItem>
+                    <SelectItem value="hst">PETROL</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </div>
+            </Select>
+            <Button className="w-1/2 mt-6">Estimate Price</Button>
+          </form>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <div className="w-1/2 h-screen bg-white "></div>
+    </div>
   );
 }
