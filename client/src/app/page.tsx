b29054d764a0be30a292c981c9e904596801ee95
Richard Shaju <richardshaju66@gmail.com>
2024-08-14 17:49:40 +0530

    Desktop ui completed

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -11,14 +11,16 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { Calendar, CarFront, Fuel, Gauge } from "lucide-react";
 
 const font = Ysabeau_SC({ subsets: ["latin"], weight: ["400"] });
 export default function Home() {
   const [brands, setBrands] = useState<[string]>();
   const [isValid, setIsValid] = useState(true);
+  const [result, setresult] = useState("");
 
   const years = [];
   for (let year = 2013; year <= 2022; year++) {
@@ -33,20 +35,47 @@ export default function Home() {
   }, []);
 
   const [formData, setFormData] = useState({
-    brand: "",
-    year: "",
-    kilometer: "",
-    fuelType: "",
+    brand: "bmw",
+    year: "2020",
+    km: "50000",
+    fuel: "petrol",
   });
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const handleKmValdator = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue: any = event.target.value;
+
+    if (newValue.length > 7) {
+      setIsValid(false);
+    } else if (newValue >= 5000 && newValue <= 100000) {
+      setFormData({ ...formData, km: newValue });
+      setIsValid(true);
+    } else {
+      setFormData({ ...formData, km: newValue });
+      setIsValid(false);
+    }
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // Access the form data here
-    console.log(formData);
+    fetch("https://used-car-price-g5fz.onrender.com/predict_car_price", {
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify({
+        ...formData,
+        fuel: formData.fuel == "petrol" ? 1 : 0,
+      }),
+      headers: {
+        "Content-type": "application/json; ",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const options = { style: "currency", currency: "INR" };
+        setresult(data.estimated_price.toLocaleString("en-IN", options));
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
   };
 
   return (
@@ -58,24 +87,34 @@ export default function Home() {
         <div className="flex flex-col justify-center items-center mt-20">
           <h1 className="text-2xl">Enter the parameters</h1>
           <form
-            className="flex flex-col gap-2 items-center"
+            className="flex flex-col gap-2 items-center mt-4"
             onSubmit={handleSubmit}
           >
             <Select>
               <div className="flex flex-col">
                 <p>brand</p>
                 <SelectTrigger className="w-[280px]">
-                  <SelectValue placeholder="Select a brand" />
+                  <SelectValue
+                    placeholder={formData.brand || "Select the brand"}
+                  />
                 </SelectTrigger>
                 <SelectContent>
                   <SelectGroup>
                     <SelectLabel>Cars</SelectLabel>
-                    {brands &&
-                      brands.map((brand: string) => (
-                        <SelectItem key={brand} value={brand}>
-                          {brand.toUpperCase()}
-                        </SelectItem>
-                      ))}
+                    {brands?.map((item) => (
+                      <div
+                        className="cursor-pointer p-1 px-8 hover:bg-gray-100"
+                        key={item.toString()}
+                        onClick={() => {
+                          setFormData((prevData) => ({
+                            ...prevData,
+                            brand: item.toString(),
+                          }));
+                        }}
+                      >
+                        {item.toString()}
+                      </div>
+                    ))}
                   </SelectGroup>
                 </SelectContent>
               </div>
@@ -85,21 +124,26 @@ export default function Home() {
               <div className="flex flex-col">
                 <p>Year</p>
                 <SelectTrigger className="w-[280px]">
-                  <SelectValue placeholder="Select Manufacture Year" />
+                  <SelectValue
+                    placeholder={formData.year || "Select Manufacture Year"}
+                  />
                 </SelectTrigger>
                 <SelectContent>
                   <SelectGroup>
                     <SelectLabel>Year</SelectLabel>
-                    {years.map((item,idx) => (
-                      <SelectItem
-                        key={item}
-                        value={formData.year}
-                        onClick={() =>
-                          setFormData({ ...formData, year: item.toString() })
-                        }
+                    {years.map((item) => (
+                      <div
+                        className="cursor-pointer p-1 px-8 hover:bg-gray-100"
+                        key={item.toString()}
+                        onClick={() => {
+                          setFormData((prevData) => ({
+                            ...prevData,
+                            year: item.toString(),
+                          }));
+                        }}
                       >
                         {item.toString()}
-                      </SelectItem>
+                      </div>
                     ))}
                   </SelectGroup>
                 </SelectContent>
@@ -111,32 +155,95 @@ export default function Home() {
               <Input
                 className="w-[280px]"
                 placeholder="Driven Kilometer"
-                name="kilometer"
-                value={formData.kilometer}
-                onChange={handleInputChange}
+                name="km"
+                value={formData.km}
+                onChange={handleKmValdator}
               />
-              {isValid ? null : <span>should be between 5000 & 100000</span>}
+              {isValid ? null : (
+                <span className="text-red-500 text-sm">
+                  should be between 5000 & 100000
+                </span>
+              )}
             </div>
             <Select>
               <div className="flex flex-col">
                 <p>fuel type</p>
                 <SelectTrigger className="w-[280px]">
-                  <SelectValue placeholder="Select a fuel type" />
+                  <SelectValue
+                    placeholder={formData.fuel || "Select Fuel Type"}
+                  />
                 </SelectTrigger>
                 <SelectContent>
                   <SelectGroup>
                     <SelectLabel>Fuel type</SelectLabel>
-                    <SelectItem value="akst">DIESEL</SelectItem>
-                    <SelectItem value="hst">PETROL</SelectItem>
+
+                    <p
+                      className="cursor-pointer p-1 px-8 hover:bg-gray-100 text-sm"
+                      onClick={() => {
+                        setFormData((prevData) => ({
+                          ...prevData,
+                          fuel: "petrol",
+                        }));
+                      }}
+                    >
+                      PETROL
+                    </p>
+                    <p
+                      className="cursor-pointer p-1 px-8 hover:bg-gray-100 text-sm"
+                      onClick={() => {
+                        setFormData((prevData) => ({
+                          ...prevData,
+                          fuel: "diesel",
+                        }));
+                      }}
+                    >
+                      DIESEL
+                    </p>
                   </SelectGroup>
                 </SelectContent>
               </div>
             </Select>
-            <Button className="w-1/2 mt-6">Estimate Price</Button>
+            <Button className={`w-1/2 mt-6" ${isValid ? '': 'bg-slate-500 cursor-not-allowed'} `} disabled={!isValid} >Estimate Price</Button>
           </form>
         </div>
       </div>
-      <div className="w-1/2 h-screen bg-white "></div>
+      <div className="w-1/2 h-screen bg-white flex justify-center items-center">
+        <div className="flex flex-col h-1/2">
+          <Image
+            src={`/logos/${formData.brand}.png`}
+            width={250}
+            height={250}
+            alt="logo"
+          />
+          <div className="flex justify-evenly gap-4 mt-8">
+            <div className="flex flex-col justify-evenly gap-4">
+              <p className="flex flex-col items-center">
+                <CarFront />
+                {formData.brand}
+              </p>
+              <p className="flex flex-col items-center">
+                <Calendar />
+                {formData.year}
+              </p>
+            </div>
+            <div className="flex flex-col  justify-evenly gap-4">
+              <p className="flex flex-col items-center">
+                <Gauge />
+                {formData.km}
+              </p>
+              <p className="flex flex-col items-center">
+                <Fuel />
+                {formData.fuel}
+              </p>
+            </div>
+          </div>
+
+          <div className="mt-3 text-center">
+            <Label className="text-lg underline">Estimated Price</Label>
+            <p className="text-xl font-mono font-semibold mt-1">{result}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
