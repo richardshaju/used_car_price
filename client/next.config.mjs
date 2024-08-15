9803993280555ab95beb415d6f714d75eb7840a8
Richard Shaju <richardshaju66@gmail.com>
2024-08-15 18:32:03 +0530

    updated next config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export",images: { unoptimized: true }}
-
-module.exports = nextConfig
+const nextConfig = { 
+    output: "export",
+    images: { unoptimized: true }
+  }
+  
+  export default nextConfig;
+  
